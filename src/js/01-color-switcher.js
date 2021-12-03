2f2baed42fff19119f89295909600e219869575b
Valerii Mazurenko <Vmazurenko@gmail.com>
2021-12-03 21:45:40 +0200

    hw9p1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,32 @@
-const btnStartChangeColor = document.querySelector('[data-start]');
-const btnStopChangeColor = document.querySelector('[data-stop]');
+const btnStartChangeColor = document.querySelector('button[data-start]');
+const btnStopChangeColor = document.querySelector('button[data-stop]');
+
 btnStartChangeColor.addEventListener('click', changeBackgroundColor);
 btnStopChangeColor.addEventListener('click', stopChangeColor);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const intervalId = setInterval(getRandomHexColor, 1000);
+
 function changeBackgroundColor() {
   document.body.style.backgroundColor = getRandomHexColor();
+  btnStartChangeColor.classList.add('active');
+  btnStopChangeColor.classList.remove('active');
+  if (btnStartChangeColor.classList.contains('active')) {
+    btnStartChangeColor.disabled = 'true';
+    btnStopChangeColor.disabled = '';
+  }
 }
+function stopChangeColor() {
+  clearInterval(intervalId);
+  btnStartChangeColor.classList.remove('active');
+  btnStopChangeColor.classList.add('active');
+  if (btnStopChangeColor.classList.contains('active')) {
+    btnStopChangeColor.disabled = 'true';
+    btnStartChangeColor.disabled = '';
+  }
+}
+// button.style.backgroundColor = 'teal';
+// button.style.fontSize = '24px';
